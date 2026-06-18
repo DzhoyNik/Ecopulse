929496092ffc0f2580ccc2b1196da898fa091d5c
Anton Mikhalev <DzhoyNik@yandex.ru>
2026-06-19 02:11:41 +0700

    feat(:domain): add repository

--- domain/src/main/java/com/example/ecopulse/domain/repository/EcoRepository.kt
+++ domain/src/main/java/com/example/ecopulse/domain/repository/EcoRepository.kt
@@ -1,2 +1,16 @@
 package com.example.ecopulse.domain.repository
 
+import com.example.ecopulse.domain.model.EcoGoal
+import com.example.ecopulse.domain.model.UserProfile
+import kotlinx.coroutines.flow.Flow
+
+interface EcoRepository {
+    // Получение профиля пользователя в реальном времени
+    fun getUserProfile(): Flow<UserProfile>
+
+    // Получение списка эко-целей для Главного экрана
+    fun getEcoGoals(): Flow<List<EcoGoal>>
+
+    // Отметка цели как выполненной
+    suspend fun completeGoal(goalId: String)
+}
